c932009eb2b8596e77bf5b6e96ba1761bd13072e
Steven Riviere <stevensteriviere@gmail.com>
2026-03-03 12:54:57 +0100

    chore: upgrade all APIs and Gateway to .NET 9, update Program.cs

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,25 +1,47 @@
+using Ocelot.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.Middleware;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var ocelotFile = builder.Environment.EnvironmentName == "Docker"
+    ? "ocelot.Docker.json"
+    : "ocelot.json";
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddJsonFile(ocelotFile, optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// JWT validation
+var issuer = builder.Configuration["Authentication:Schemes:Bearer:ValidIssuer"];
+var audience = builder.Configuration["Authentication:Schemes:Bearer:ValidAudience"];
+var secret = builder.Configuration["Authentication:Schemes:Bearer:Secret"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret!))
+        };
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
